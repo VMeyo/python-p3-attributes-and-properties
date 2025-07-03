e5488c8ce81cb7165977073e4b64a6370a8be17a
Victor <victor.meyo@student.moringaschool.com>
2025-07-03 10:55:40 +0300

    Additional comments added on the person class

--- lib/person.py
+++ lib/person.py
@@ -22,18 +22,20 @@ class Person:
         "Legal", "Finance", "Sales", "General Management", "Research & Development",
         "Marketing", "Purchasing"
     ]
-    
+    # class attribute
+
     def __init__(self, name="John Doe", job="Admin"):
-        self.name = name
-        self.job = job
+        self.name = name # Instance attribute
+        self.job = job # Instance attribute
 
     @property
     def name(self):
         return self._name
+    # Allows us to access name as an attribute
 
-    @name.setter
+    @name.setter # Validates the name
     def name(self, value):
-        if isinstance(value, str) and 1 <= len(value) <= 25:
+        if isinstance(value, str) and 1 <= len(value) <= 25: # Ensures the name is between 1 & 25 characters.
             self._name = value.title()
         else:
             print("Name must be string between 1 and 25 characters.")
@@ -41,10 +43,11 @@ class Person:
     @property
     def job(self):
         return self._job
+    # Allows us to access job as an attribute
 
-    @job.setter
+    @job.setter # Validates the job
     def job(self, value):
-        if value in Person.approved_jobs:
+        if value in Person.approved_jobs: # Class variable is can be called by the class
             self._job = value
         else:
             print("Job must be in list of approved jobs.")
